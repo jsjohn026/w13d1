8ebb03bd52a70260f28ff9c6b177fc024d2d5c07
Jasmine <jsjohn026@gmail.com>
2019-05-06 14:30:18 -0700

    initial commit widgets react project

---
+++ frontend/widgets.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import Clock from './clock';
+
+const Root = () => (
+     <div> 
+       <Clock />
+     </div>);
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+   ReactDOM.render(<Root />, document.getElementById("main"));  
+});
